3c5edaaf160a22d8dad0d3176f9e255130128096
nishant51 <timsinanishant51@gmail.com>
2024-06-10 22:01:24 +0545

    major api completed

--- product/admin.py
+++ product/admin.py
@@ -1,10 +1,37 @@
 from django.contrib import admin
 from .models import Product, SoldProduct, DailySalesReport
+from .models import Category, SubCategory
+
+class SubCategoryInline(admin.TabularInline):
+    model = SubCategory
+    extra = 1
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+    inlines = [SubCategoryInline]
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category')
+    search_fields = ('name', 'category__name')
+    list_filter = ('category',)
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'quantity', 'added_date', 'updated_date', 'empty_date')
-    search_fields = ('name',)
+    list_display = ('id', 'name', 'price', 'quantity', 'added_date', 'updated_date', 'empty_date', 'category', 'sub_category')
+    search_fields = ('name', 'category__name', 'sub_category__name')
+    list_filter = ('category', 'sub_category')
+    readonly_fields = ('added_date', 'updated_date')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'price', 'quantity', 'category', 'sub_category')
+        }),
+        ('Dates', {
+            'fields': ('added_date', 'updated_date', 'empty_date')
+        }),
+    )
 
 @admin.register(SoldProduct)
 class SoldProductAdmin(admin.ModelAdmin):
--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .viewsets import ProductViewSet, SoldProductViewSet, DailySalesReportViewSet
+from .viewsets import CategoryViewSet, ProductViewSet, SoldProductViewSet, DailySalesReportViewSet, SubCategoryViewSet
 
 router = DefaultRouter()
+
+router.register(r'category', CategoryViewSet, basename='category')
+router.register(r'sub-category', SubCategoryViewSet, basename='sub-catergory')
 router.register(r'products', ProductViewSet, basename='product')
 router.register(r'soldproducts', SoldProductViewSet, basename='soldproduct')
 router.register(r'dailysalesreports', DailySalesReportViewSet, basename='dailysalesreport')
