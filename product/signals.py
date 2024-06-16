d7a43fbba0f827c10804dfd09f038feac26b32e7
nishant51 <timsinanishant51@gmail.com>
2024-06-16 20:40:32 +0545

    abc

--- product/signals.py
+++ product/signals.py
@@ -1,12 +1,16 @@
-# your_app/signals.py
-
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import InvoiceItem
 
-@receiver(post_save, sender=InvoiceItem)
-def reduce_product_stock(sender, instance, created, **kwargs):
-    if created:
+@receiver(pre_save, sender=InvoiceItem)
+def update_product_stock(sender, instance, **kwargs):
+    if instance.pk:
+        previous_instance = InvoiceItem.objects.get(pk=instance.pk)
+        product = instance.product
+        difference = instance.quantity - previous_instance.quantity
+        product.in_stock -= difference
+        product.save()
+    else:
         product = instance.product
         product.in_stock -= instance.quantity
         product.save()
