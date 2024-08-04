b78f4cf2cc2cca1d55d44c933fd4ccbb90663791
nishant51 <timsinanishant51@gmail.com>
2024-08-04 11:30:32 +0545

    aaa

--- project/urls.py
+++ project/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('api/product/',include('product.urls'))
 
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+ 
